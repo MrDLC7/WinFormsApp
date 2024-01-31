d390d10e0c8f273b3e5b8a2415dc84b948fd640d
MrDLC7 <mafia78ton@gmail.com>
2024-01-31 18:49:23 +0200

    develop #1

--- WinFormsAppExcel/MainForm.Designer.cs
+++ WinFormsAppExcel/MainForm.Designer.cs
@@ -29,45 +29,113 @@
         private void InitializeComponent()
         {
             groupBox = new GroupBox();
-            dataGridView = new DataGridView();
             buttonOpenFile = new Button();
+            textBoxColumn = new TextBox();
+            textBoxRow = new TextBox();
+            labelColumn = new Label();
+            labelRow = new Label();
+            groupBox1 = new GroupBox();
+            dataGridView = new DataGridView();
             openFileDialog = new OpenFileDialog();
             groupBox.SuspendLayout();
+            groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView).BeginInit();
             SuspendLayout();
             // 
             // groupBox
             // 
             groupBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            groupBox.Controls.Add(dataGridView);
+            groupBox.AutoSize = true;
+            groupBox.BackColor = SystemColors.ControlLight;
             groupBox.Controls.Add(buttonOpenFile);
-            groupBox.Location = new Point(11, -6);
+            groupBox.Controls.Add(textBoxColumn);
+            groupBox.Controls.Add(textBoxRow);
+            groupBox.Controls.Add(labelColumn);
+            groupBox.Controls.Add(labelRow);
+            groupBox.Controls.Add(groupBox1);
+            groupBox.Location = new Point(0, 0);
             groupBox.Name = "groupBox";
-            groupBox.Size = new Size(778, 444);
+            groupBox.Size = new Size(800, 450);
             groupBox.TabIndex = 0;
             groupBox.TabStop = false;
-            // 
-            // dataGridView
-            // 
-            dataGridView.Anchor = AnchorStyles.None;
-            dataGridView.BackgroundColor = SystemColors.ButtonHighlight;
-            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView.Location = new Point(6, 87);
-            dataGridView.Name = "dataGridView";
-            dataGridView.RowHeadersWidth = 51;
-            dataGridView.Size = new Size(766, 351);
-            dataGridView.TabIndex = 1;
             // 
             // buttonOpenFile
             // 
             buttonOpenFile.Anchor = AnchorStyles.Top;
-            buttonOpenFile.BackColor = SystemColors.ButtonFace;
-            buttonOpenFile.Location = new Point(231, 19);
+            buttonOpenFile.BackColor = SystemColors.ButtonHighlight;
+            buttonOpenFile.Location = new Point(463, 24);
             buttonOpenFile.Name = "buttonOpenFile";
             buttonOpenFile.Size = new Size(320, 56);
             buttonOpenFile.TabIndex = 0;
-            buttonOpenFile.Text = "Відкрити документ Excel";
+            buttonOpenFile.Text = "Завантажити документ Excel";
             buttonOpenFile.UseVisualStyleBackColor = false;
+            buttonOpenFile.Click += buttonOpenFile_Click;
+            // 
+            // textBoxColumn
+            // 
+            textBoxColumn.Anchor = AnchorStyles.Top;
+            textBoxColumn.Location = new Point(155, 58);
+            textBoxColumn.Name = "textBoxColumn";
+            textBoxColumn.Size = new Size(118, 27);
+            textBoxColumn.TabIndex = 5;
+            textBoxColumn.Text = "I";
+            textBoxColumn.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBoxRow
+            // 
+            textBoxRow.Anchor = AnchorStyles.Top;
+            textBoxRow.Location = new Point(155, 22);
+            textBoxRow.Name = "textBoxRow";
+            textBoxRow.Size = new Size(118, 27);
+            textBoxRow.TabIndex = 4;
+            textBoxRow.Text = "5";
+            textBoxRow.TextAlign = HorizontalAlignment.Center;
+            // 
+            // labelColumn
+            // 
+            labelColumn.Anchor = AnchorStyles.Top;
+            labelColumn.AutoSize = true;
+            labelColumn.Location = new Point(17, 60);
+            labelColumn.Name = "labelColumn";
+            labelColumn.Size = new Size(97, 20);
+            labelColumn.TabIndex = 3;
+            labelColumn.Text = "По колонку:";
+            // 
+            // labelRow
+            // 
+            labelRow.Anchor = AnchorStyles.Top;
+            labelRow.AutoSize = true;
+            labelRow.Location = new Point(17, 24);
+            labelRow.Name = "labelRow";
+            labelRow.Size = new Size(81, 20);
+            labelRow.TabIndex = 2;
+            labelRow.Text = "По рядок:";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            groupBox1.Controls.Add(dataGridView);
+            groupBox1.Location = new Point(7, 84);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(787, 358);
+            groupBox1.TabIndex = 6;
+            groupBox1.TabStop = false;
+            // 
+            // dataGridView
+            // 
+            dataGridView.AllowUserToAddRows = false;
+            dataGridView.AllowUserToDeleteRows = false;
+            dataGridView.AllowUserToResizeColumns = false;
+            dataGridView.AllowUserToResizeRows = false;
+            dataGridView.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView.BackgroundColor = SystemColors.ButtonHighlight;
+            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView.Location = new Point(3, 23);
+            dataGridView.Name = "dataGridView";
+            dataGridView.ReadOnly = true;
+            dataGridView.RowHeadersWidth = 51;
+            dataGridView.Size = new Size(781, 332);
+            dataGridView.TabIndex = 2;
             // 
             // openFileDialog
             // 
@@ -80,17 +148,25 @@
             ClientSize = new Size(800, 450);
             Controls.Add(groupBox);
             Name = "MainForm";
-            Text = "Form1";
+            Text = "Excel export to Table";
             groupBox.ResumeLayout(false);
+            groupBox.PerformLayout();
+            groupBox1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)dataGridView).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private GroupBox groupBox;
         private Button buttonOpenFile;
-        private DataGridView dataGridView;
         private OpenFileDialog openFileDialog;
+        private TextBox textBoxColumn;
+        private TextBox textBoxRow;
+        private Label labelColumn;
+        private Label labelRow;
+        private GroupBox groupBox1;
+        private DataGridView dataGridView;
     }
 }
--- WinFormsAppExcel/MainForm.cs
+++ WinFormsAppExcel/MainForm.cs
@@ -1,3 +1,8 @@
+using Aspose.Cells;
+using System.Data;
+using System.Reflection;
+
+
 namespace WinFormsAppExcel
 {
     public partial class MainForm : Form
@@ -5,6 +10,61 @@ namespace WinFormsAppExcel
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            int row = 0;
+            int column = 0;
+            try
+            {
+                row = Convert.ToInt32(textBoxRow.Text);
+                column = ConvertColumnExcelToInt(textBoxColumn.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            LoadExcelData(row, column);
+        }
+
+        private void LoadExcelData(int row, int column)
+        {
+            try
+            {
+                OpenFileDialog fileExcel = new OpenFileDialog();
+
+                fileExcel.InitialDirectory = "";
+                fileExcel.DefaultExt = "*.xls;*.xlsx";
+                fileExcel.Filter = "Excel Sheet(*.xlsx)|*.xlsx";
+                fileExcel.Title = "Виберіть документ";
+                fileExcel.RestoreDirectory = true;
+
+                if (fileExcel.ShowDialog() == DialogResult.OK && fileExcel.FileName.Length > 0)
+                {
+                    string path = fileExcel.FileName;
+                    FileStream fstream = new FileStream(path, FileMode.Open);
+                    Workbook workbook = new Workbook(fstream);
+                    Worksheet worksheet = workbook.Worksheets[0];
+                    DataTable table = worksheet.Cells.ExportDataTable(0, 0, row, column, true);
+                    dataGridView.DataSource = table;
+                    fstream.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private int ConvertColumnExcelToInt(string str)
+        {
+            int num = 0;
+            foreach (char c in str)
+            {
+                num = num * 26 + (c - '@');
+            }
+            return num;
         }
     }
 }
