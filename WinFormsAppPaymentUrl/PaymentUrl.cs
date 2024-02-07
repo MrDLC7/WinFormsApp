5edb7fe3a8bf968059b7c94820f6853c56c08e71
MrDLC7 <mafia78ton@gmail.com>
2024-02-07 22:51:37 +0200

    Start #9

--- WinFormsAppPaymentUrl/PaymentUrl.cs
+++ WinFormsAppPaymentUrl/PaymentUrl.cs
@@ -11,9 +11,12 @@ namespace WinFormsAppPaymentUrl
             InitializeComponent();
         }
 
+        private int row = 0, column = 0;
+        private string path = string.Empty;
+
         private void btnOpenFile_Click(object sender, EventArgs e)
         {
-            int row = 0, column = 0;
+
             try
             {
                 // Створення вікна вибору файлу Excel
@@ -31,32 +34,8 @@ namespace WinFormsAppPaymentUrl
                 if (fileExcel.ShowDialog() == DialogResult.OK && fileExcel.FileName.Length > 0)
                 {
                     // Шлях до файлу
-                    string path = fileExcel.FileName;
-
-                    // Створення файлового потоку, що містити Excel, який потрібно перевірити
-                    FileStream fstream = new FileStream(path, FileMode.Open);
-
-                    // Створення екземпляру об'єкта Workbook
-                    Workbook workbook = new Workbook(fstream);
-
-                    // Доступ до першого в файлі Excel
-                    Worksheet worksheet = workbook.Worksheets[0];
-                    
-                    // Макс. кількість рядків і колонок
-                    row = worksheet.Cells.MaxDataRow + 1;
-                    column = worksheet.Cells.MaxDataColumn + 1;
-
-                    // Створення екземпляру класу DataTable для збереження даних
-                    DataTable table = new DataTable();
-
-                    // Заповнення DataTable даними з Excel
-                    table = worksheet.Cells.ExportDataTable(0, 0, row, column, true);
-
-                    // Встановлення DataTable як джерела даних для DataGridView
-                    dataGridView.DataSource = table;
-
-                    // Закриття файлового потоку
-                    fstream.Close();
+                    path = fileExcel.FileName;
+                    OpenFileExcel();
                 }
             }
             catch (Exception ex)
@@ -65,10 +44,100 @@ namespace WinFormsAppPaymentUrl
             }
         }
 
-
         private void btnUpdateDataFile_Click(object sender, EventArgs e)
         {
 
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            UpdateFileExcel();
         }
+
+        private void OpenFileExcel()
+        {
+            try
+            {
+                // Створення файлового потоку, що містити Excel, який потрібно перевірити
+                FileStream fstream = new FileStream(path, FileMode.Open);
+
+                // Створення екземпляру об'єкта Workbook
+                Workbook workbook = new Workbook(fstream);
+
+                // Доступ до першого в файлі Excel
+                Worksheet worksheet = workbook.Worksheets[0];
+
+                // Макс. кількість рядків і колонок
+                row = worksheet.Cells.MaxDataRow + 1;
+                column = worksheet.Cells.MaxDataColumn + 1;
+
+                // Створення екземпляру класу DataTable для збереження даних
+                DataTable table = new DataTable();
+
+                // Заповнення DataTable даними з Excel
+                table = worksheet.Cells.ExportDataTable(0, 0, row, column, true);
+                // Встановлення DataTable як джерела даних для DataGridView
+                dataGridView.DataSource = table;
+
+                // Закриття файлового потоку
+                fstream.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void UpdateFileExcel()
+        {
+            try
+            {
+                // Створення файлового потоку, що містити Excel, який потрібно перевірити
+                FileStream fstream = new FileStream(path, FileMode.Open);
+
+                // Створення екземпляру об'єкта Workbook
+                Workbook workbook = new Workbook(fstream);
+
+                // Доступ до першого в файлі Excel
+                Worksheet worksheet = workbook.Worksheets[0];
+
+                // Макс. кількість рядків і колонок
+                row = worksheet.Cells.MaxDataRow + 1;
+                column = worksheet.Cells.MaxDataColumn + 1;
+
+                // Створення екземпляру класу DataTable для збереження даних
+                DataTable dataTable = new DataTable();
+
+                // Встановлення DataTable як джерела даних для DataGridView
+                foreach (DataGridViewColumn column in dataGridView.Columns)
+                {
+                    dataTable.Columns.Add(column.HeaderText, typeof(string));
+                }
+                foreach (DataGridViewRow row in dataGridView.Rows)
+                {
+                    DataRow dataRow = dataTable.NewRow();
+                    foreach (DataGridViewCell cell in row.Cells)
+                    {
+                        dataRow[cell.ColumnIndex] = cell.Value;
+                    }
+                    dataTable.Rows.Add(dataRow);
+                }
+
+                // Заповнення DataTable даними з Excel
+                worksheet.Cells.ImportDataTable(dataTable, true, 0, 0);
+                workbook.Save(path);
+
+                // Закриття файлового потоку
+                fstream.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    
     }
 }
